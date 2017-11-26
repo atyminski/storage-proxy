5d7e5871b074c41174946616f63b6dcc496aa52d
Arkadiusz Tymiński <gevleeog@gmail.com>
2017-11-26 12:06:24 +0100

    logging and config

--- cmd/server/app/run.go
+++ cmd/server/app/run.go
@@ -1,40 +1,67 @@
 package app
 
-import(
+import (
+	"fmt"
 	"net/http"
-	"github.com/spf13/cobra"
-	"github.com/gorilla/mux"
+	"os"
+
+	"github.com/sirupsen/logrus"
+
 	"github.com/gevlee/storage-proxy/internal/pkg/handlers"
+
+	"github.com/gorilla/mux"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-var(
-	port, scope string
-)
-
+var configPath string
 var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Runs storage proxy server",
-	Long: `Runs storage proxy server`,
+	Long:  `Runs storage proxy server`,
 	Run: func(cmd *cobra.Command, args []string) {
-		runServer();
-	 },
+		runServer()
+	},
 }
 
 func runServer() {
-	handler := createHandler();
+	port := viper.GetString("port")
 	r := mux.NewRouter()
+	initRouter(r)
+	http.Handle("/", r)
+	logrus.WithFields(logrus.Fields{
+		"port": port,
+	}).Info("Server run")
+	logrus.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func initRouter(r *mux.Router) {
+	handler := createHandler()
 	r.HandleFunc("/files/{context}/{fileid}", handler.Upload).Methods("POST")
 	r.HandleFunc("/files/{context}/{fileid}", handler.Download).Methods("GET")
 	r.HandleFunc("/files/{context}/{fileid}", handler.Remove).Methods("DELETE")
-	http.Handle("/", r)
-	http.ListenAndServe(":" + port, nil)
 }
 
-func createHandler() handlers.IHandler{
-	return handlers.NewFileSystemHandler(scope)
+func createHandler() handlers.IHandler {
+	return handlers.NewFileSystemHandler(viper.GetString("scope"))
 }
 
-func init(){
-	runCmd.PersistentFlags().StringVar(&port, "port", "8000", "Server port")
-	runCmd.PersistentFlags().StringVar(&scope, "scope", ".", "Server port")
+func init() {
+	cobra.OnInitialize(initConfig)
+	runCmd.Flags().String("port", "8000", "Server port")
+	runCmd.Flags().String("scope", ".", "Server port")
+	runCmd.Flags().StringVar(&configPath, "config", "", "Path to config file")
+	viper.BindPFlag("port", runCmd.Flags().Lookup("port"))
+	viper.BindPFlag("scope", runCmd.Flags().Lookup("scope"))
+}
+
+func initConfig() {
+	if configPath != "" {
+		viper.SetConfigType("yaml")
+		viper.SetConfigFile(configPath)
+		if err := viper.ReadInConfig(); err != nil {
+			fmt.Println("Can't read config:", err)
+			os.Exit(1)
+		}
+	}
 }
--- cmd/server/app/server.go
+++ cmd/server/app/server.go
@@ -1,21 +1,31 @@
 package app
 
-import(
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Storage proxy server",
-	Long: `Storage proxy server`,
+	Long:  `Storage proxy server`,
 	Run: func(cmd *cobra.Command, args []string) {
-		 cmd.Help();
-	 },
+		cmd.Help()
+	},
 }
 
-func init(){
+func init() {
+	cobra.OnInitialize(initLog)
 	rootCmd.AddCommand(runCmd)
 	rootCmd.AddCommand(versionCmd)
+}
+
+func initLog() {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.InfoLevel)
 }
 
 func Execute() {
